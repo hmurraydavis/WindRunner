ed984c7d91d477bf42ab5f80258eec888f46238c
Halie <hmurraydavis@users.noreply.github.com>
2014-06-12 10:18:46 -0400

    Create mtnHeading.py

---
+++ mtnHeading.py
@@ -0,0 +1,22 @@
+def mtnHeading():
+	# Keeps the robot on a desired heading 
+	import bank
+	currentHeading=bank.bank('currentHeading')
+	desiredHeading=bank.bank('desiredHeading')
+	print 'Current heading: ' + str(currentHeading)
+	print 'Desired heading:' +str(desiredHeading)
+	delta=abs((currentHeading-desiredHeading)/2)
+	print 'Delta: '+ str(delta)
+	if currentHeading > desiredHeading:
+		desiredStearing=currentHeading-delta
+		print 'desired greater than current'
+	if currentHeading<desiredHeading:
+		desiredStearing=currentHeading+delta
+		print 'current greater than desired'
+	
+	print 'the desired stearing angle is: ' + str(desiredStearing)
+	return desiredStearing
+
+
+
+mtnHeading()
