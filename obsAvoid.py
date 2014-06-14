ee456098cad66e76faaceb0044094e89664b2585
Halie <hmurraydavis@users.noreply.github.com>
2014-06-14 03:37:11 -0400

    Changed to void and added function call to bank to get current data

--- obsAvoid.py
+++ obsAvoid.py
@@ -1,10 +1,13 @@
-def obsAvoid(dist_to_object):
+def obsAvoid():
 	#works with PIR sensor to avoid obstacles. Essentially integral control.
 	import bank
+	
+	dist_to_object=bank.bank('dist_to_object') #distance to object from PIR/Sharp sensor
 	if  dist_to_object >50:
 		print "Servo moving necessary so I don't hit a rock!"
 		stearingDesired = 30*dist_to_object/256; 
-		sailDesired = bank.bank('sailDesired') + 1
+		#sailDesired = bank.bank('sailDesired') + 1
+		sailDesired=bank.sailDesired()
 		print 'desired stearing is: ' + str(stearingDesired) + ' degrees'
 		print 'desired sail is: ' + str(sailDesired) + ' degrees'
 		return (stearingDesired, sailDesired)
@@ -13,4 +16,4 @@ def obsAvoid(dist_to_object):
 
 
 
-obsAvoid(100)
+obsAvoid()
