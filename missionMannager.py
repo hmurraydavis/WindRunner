ea55503759874fcf89ea0ae70e56e89e79d855bf
Halie <hmurraydavis@users.noreply.github.com>
2014-06-16 10:49:54 -0400

    added mission handling

--- missionMannager.py
+++ missionMannager.py
@@ -1,4 +1,13 @@
+import time
+import bank
+import followLine
+import obsAvoid
+import goToPoint
+import mtnHeading
+
+
 def missionMannager():
+	
 	number_of_modes=int(raw_input('How many modes would you like to have in your mission?'))
 	
 	mission=[]
@@ -11,8 +20,8 @@ def missionMannager():
 			mission.append(['line follow',pt1,pt2])
 			
 		if mode=='obstacle avoid':
-			time=raw_input('How long would you like it to avoid obstacles on its own?')
-			mission.append(['obstacle avoid',time])
+			timeT=raw_input('How long would you like it to avoid obstacles on its own?')
+			mission.append(['obstacle avoid',timeT])
 			
 		if mode=='go to point':
 			pt=raw_input('GPS coordinates of point (in brackets:[])? ')
@@ -20,16 +29,26 @@ def missionMannager():
 			
 		if mode=='maintain heading':
 			heading=raw_input('Desired global heading (in degrees)?')
-			mission.append(['maintain heading',heading])
+			timeT=timeT=raw_input('How long would you like it to maintain this heading?')
+			mission.append(['maintain heading',heading,timeT])
 			
 	print 'Mission is:', mission
+	
 	for mis in mission:
-		if mis=='line follow':
-			pass
-		if mis=='obstacle avoid':
-			pass
-		if mis=='go to point':
-			pass
-		if mis=='maintain heading':
-			pass
+		print mis[0]
+		if mis[0]=='line follow':
+			while bank.bank('posCurrent')!=mis[2]:
+				followLine.followLine()
+				time.sleep(.25)
+		if mis[0]=='obstacle avoid':
+			for t in range (0,2*int(mis[1])):
+				obsAvoid.obsAvoid()
+		if mis[0]=='go to point':
+			while bank.bank('posCurrent')!=mis[1]:
+				goToPoint.goToPoint()
+				time.sleep(.25)
+		if mis[0]=='maintain heading':
+			for t in range (0,4*int(mis[2])):
+				mtnHeading.mtnHeading()
+				time.sleep(.25)
 missionMannager()
