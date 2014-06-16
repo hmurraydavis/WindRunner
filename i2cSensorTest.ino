eb92cc2a8a4ce7c6fa7af02ca752b470689f1b57
Halie <hmurraydavis@users.noreply.github.com>
2014-06-16 12:05:12 -0400

    no work. :(

--- i2cSensorTest.ino
+++ i2cSensorTest.ino
@@ -11,14 +11,19 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly: 
   Wire.beginTransmission(0X3D);
-  char byte1=Wire.read();
-  char byte2=Wire.read();
-  char byte3=Wire.read();
+  Wire.write(1);
+  //char byte1=Wire.read();
+  //char byte2=Wire.read();
+  //char byte3=Wire.read();
   Wire.endTransmission();
   
+  Wire.requestFrom(0X3D, 1);
+  Serial.println(Wire.read());
+ 
+  
   Serial.println('Transmission');
-  Serial.println(byte1);
-  Serial.println(byte2);
-  Serial.println(byte3);
+  //Serial.println(String(byte1));
+  //Serial.println(byte2);
+  //Serial.println(byte3);
   
 }
