4b020c7a6eada34a248a2fe7dfdbd684c33891cc
Halie <hmurraydavis@users.noreply.github.com>
2014-06-19 14:42:30 -0400

    Delete sailDesToServoDes.py

--- sailDesToServoDes.py
+++
@@ -1,9 +0,0 @@
-def sailDesToServoDes():
-	#midbrain
-	#converts from the sail theta to the equivelent theta for the sail winch
-	import bank
-	sailDesired=bank.bank('sailDesired')
-	sailServoDesired= (sailDesired-20)/2
-	print 'The servo set angle is: ' +str(sailServoDesired) + ' degrees'
-	return sailServoDesired
-sailDesToServoDes()
