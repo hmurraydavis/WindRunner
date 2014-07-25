410065930aff5ae27856dedadc2524b55d7c4774
Halie Murray-Davis <hmurraydavis@yahoo.com>
2014-07-25 19:33:32 +0000

    Real GPS data read in

--- Together-versions/HindBrain.py
+++ Together-versions/HindBrain.py
@@ -5,13 +5,13 @@ import binascii
 class Hindbrain:
 	
 	def __init__(self):
-		#self.GPS=serial.Serial ('/dev/ttyO1') #uncomment when on BB
+		self.GPS=serial.Serial ('/dev/ttyO1') #uncomment when on BB
 		
 		#Arduino serial connection
-		self.arduino=serial.Serial('/dev/ttyACM0') #uncomment when arduino is connected
+		#self.arduino=serial.Serial('/dev/ttyACM0') #uncomment when arduino is connected
 		
 		self.count=0
-		pass # comment when GPS or Arduino is connected
+		#pass # comment when GPS or Arduino is connected
 	
 	def moveSailServos(self,sailServoAngle):
 		# '''Moves the sail winch servo to the desired location'''
@@ -29,7 +29,7 @@ class Hindbrain:
 		pass
 		
 	def moveStearServo(self, stearServoAngle):
-		# '''Moves the stearing servo to the desired angle'''
+		#'''Moves the stearing servo to the desired angle'''
 		send='s%i\n' %stearServoAngle
 		self.arduino.write(send)
 		print send
@@ -44,7 +44,7 @@ class Hindbrain:
 		pass
 		
 	def readTilt(self):
-		# '''Read the current tild of the robot off the horizontal from the gyroscope'''
+		'''Read the current tild of the robot off the horizontal from the gyroscope'''
 		send='g\n'
 		self.arduino.write(send)
 		print send
@@ -60,53 +60,55 @@ class Hindbrain:
 		pass
 		
 	def readPosition(self):
-		# '''Reads the current GPS position from the GPS chip.'''
-		#data=self.GPS.readline() #uncomment when on BB
+		'''Reads the current GPS position from the GPS chip.'''
+		data=self.GPS.readline() #uncomment when on BB
 		
 		#test data set: 
-		data='$GPGGA,001038.00,3334.2313457,S,11211.0576940,W,2,04,5.4,354.682,M,-26.574,M,7.0,0138*74' #comment out when on BB
-		ckStng=data[1:data[14].find('*')-2]
-
-		ckSum=0
-		for i in ckStng:
-			ckSum = ckSum ^ ord(i)
-		#print 'Check sum caluclated: hex:', hex(ckSum), 'dec:', ckSum
-		data=data.split(',')
-		ckSumSat=data[14].split('*')
-
+		#data='$GPGGA,001038.00,3334.2313457,S,11211.0576940,W,2,04,5.4,354.682,M,-26.574,M,7.0,0138*74' #comment out when on BB
 		
-		#if str(hex(ckSum)) != '0x'+str(ckSumSat[1]): #use  check sum from GPS vs calculated to verify string isn't corrupted:
-			#print 'Check Sum Error from the GPS! :('
-			#positionUse=bank.bank('posPast')
-			#return positionUse
+		if data.startswith('$GPGGA')==True:
+			ckStng=data[1:data[14].find('*')-2]
+
+			ckSum=0
+			for i in ckStng:
+				ckSum = ckSum ^ ord(i)
+			print 'Check sum caluclated: hex:', hex(ckSum), 'dec:', ckSum
+			data=data.split(',')
+			ckSumSat=data[14].split('*')
+	
 			
-		if False: #TODO: uncomment above checksum stuff, delete this placeholder
-			pass 
-			
-		else: #if the GPS string isn't corrupted, use it:
-			print 'Good GPS data! :)'					
-			if data[6]==0: #if there isn't a GPS lock, assume the robot hasn't moved
-				print 'No GPS Lock! :('
-				positionUse=bank.bank('posPast')
-				return positionUse
+			#if str(hex(ckSum)) != '0x'+str(ckSumSat[1]): #use  check sum from GPS vs calculated to verify string isn't corrupted:
+				#print 'Check Sum Error from the GPS! :('
+				#positionUse=bank.bank('posPast')
+				#return positionUse
 				
-			if data[6]==1:
-				print 'GPS fix!'
-			if data[6]==2:
-				print 'DGPS fix!'
+			if False: #TODO: uncomment above checksum stuff, delete this placeholder
+				pass 
 				
-			latitude=data[2]
-			longitude=data[4]
-			latitude=int(latitude[:2])+(float(latitude[2:])/60)
-			if data[3]=='S': #convert to negative if on the negative side of the earth
-				latitude=latitude*-1
-			longitude=int(longitude[:2])+(float(longitude[2:])/60)
-			if data[5]=='W': #convert to negative if on the negative side of the world
-				longitude=longitude*-1
-			print 'Processed latitude:',latitude
-			print 'Processed longitude:',longitude
-			#return [latitude,longitude]	#TODO: uncomment when are actually using GPS data
-			return [0,1]
+			else: #if the GPS string isn't corrupted, use it:
+				print 'Good GPS data! :)'					
+				if data[6]==0: #if there isn't a GPS lock, assume the robot hasn't moved
+					print 'No GPS Lock! :('
+					positionUse=bank.bank('posPast')
+					return positionUse
+					
+				if data[6]==1:
+					print 'GPS fix!'
+				if data[6]==2:
+					print 'DGPS fix!'
+				
+				latitude=data[2]
+				longitude=data[4]
+				latitude=int(latitude[:2])+(float(latitude[2:])/60)
+				if data[3]=='S': #convert to negative if on the negative side of the earth
+					latitude=latitude*-1
+				longitude=int(longitude[:2])+(float(longitude[2:])/60)
+				if data[5]=='W': #convert to negative if on the negative side of the world
+					longitude=longitude*-1
+				print 'Processed latitude:',latitude
+				print 'Processed longitude:',longitude
+				#return [latitude,longitude]	#TODO: uncomment when are actually using GPS data
+				return [0,1]
 		
 		
 	def readHeading(self):
@@ -171,9 +173,9 @@ class Hindbrain:
 if __name__=='__main__':
 	HB=Hindbrain()
 	HB.readPosition()
-	HB.moveSailServos(40)
-	HB.moveStearServo(50)
-	HB.readTilt()
-	HB.readHeading()
-	HB.readObstacle1()
-	HB.readWindDirecrion()
+	#HB.moveSailServos(40)
+	#HB.moveStearServo(50)
+	#HB.readTilt()
+	#HB.readHeading()
+	#HB.readObstacle1()
+	#HB.readWindDirecrion()
