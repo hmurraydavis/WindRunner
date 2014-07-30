16e2cf3ab1efc67ea4dd9070d6a9dbb25be300d2
Halie <hmurraydavis@users.noreply.github.com>
2014-07-30 14:36:07 -0400

    Outputs 129

--- i2cSensorTest.ino
+++ i2cSensorTest.ino
@@ -20,25 +20,7 @@ void wakeUpGyroY(){
   
   //Now time to write the new value to pow_mgmt_2 register
   //to wake the gyro y axis up
-  int pwrMgmt2ValueForGyroY=B00000000;
-  Wire.beginTransmission(gyroAddress);
-  Wire.write(pwrMgmt2ValueForGyroY);
-  Wire.endTransmission();
-}
-
-void bedTimeGyro(){
-  //Tell the gyroscope we're talking to it and that 
-  //we're going to change the pow_mgmt_2 register to
-  //wake it up. 
-  int gyroAddress=0x68;
-  int powerMgmt2Address=0x6C;
-  Wire.beginTransmission(gyroAddress);
-  Wire.write(powerMgmt2Address);
-  Wire.endTransmission();
-  
-  //Now time to write the new value to pow_mgmt_2 register
-  //to wake the gyro y axis up
-  int pwrMgmt2ValueForGyroY=B00000000;
+  int pwrMgmt2ValueForGyroY=B00000010;
   Wire.beginTransmission(gyroAddress);
   Wire.write(pwrMgmt2ValueForGyroY);
   Wire.endTransmission();
@@ -55,15 +37,13 @@ void loop() {
   //char byte3=Wire.read();
   Wire.endTransmission();
   
-  //wakeUpGyroY();//wake up the gyroscope before trying to read from it
+  wakeUpGyroY();//wake up the gyroscope before trying to read f
   
   Wire.requestFrom(gyroAddress, 1);
   Serial.println(Wire.read());
  
   
   Serial.println("Transmission");
-  //bedTimeGyro();
-  
   //Serial.println(String(byte1));
   //Serial.println(byte2);
   //Serial.println(byte3);
