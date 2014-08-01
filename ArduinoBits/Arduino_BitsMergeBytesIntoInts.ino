cb26a4894b10276d9acadab45211ad2307d1a1f7
Halie <hmurraydavis@users.noreply.github.com>
2014-08-01 14:53:47 -0400

    Move file

---
+++ ArduinoBits/Arduino_BitsMergeBytesIntoInts.ino
@@ -0,0 +1,17 @@
+#include <SoftwareSerial.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly: 
+  int bigNo=674;
+  byte readInYGyroL=lowByte(bigNo);
+  byte readInYGyroH=highByte(bigNo);
+  int gyroYReading=(int)(readInYGyroL+(readInYGyroH<<8));
+  
+  Serial.println("Big no is: " + String(bigNo));
+  Serial.println("Big reconstructed is: "+String(gyroYReading));
+}
