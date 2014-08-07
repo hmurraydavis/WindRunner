2e02a298d4c93e3c2358007b6336eaca66217603
Halie Murray-Davis <hmurraydavis@yahoo.com>
2014-08-07 16:25:47 -0400

    Works with python code for tilt, heading, and wind direction

--- Together-versions/windRunnerHindBrain/windRunnerHindBrain.ino
+++ Together-versions/windRunnerHindBrain/windRunnerHindBrain.ino
@@ -8,34 +8,8 @@ char current_line[8]; // allocate some space for the string
 
 //declare steering servo:
 Servo steerServo;
-//Declare sail Servo
+//Declare sail Servo:
 Servo sailServo;
-
-
-//Sensor & actuator read/write functions:
-String compassRead(){}
-
-String irRead(){
-    int irSensor = 0; //IR sensor is on Arduino Analog pin 0
-    int irReading=analogRead(irSensor);
-    
-    return String(irReading);
-}
-
-String gyroscopeRead(){}
-
-String windRead(){
-	int windDirection=50; //TODO: actually read in wind direction
-	return String(windDirection);
-}
-
-void steerServoSet(int steerServoHeading){ 
-    steerServo.write(steerServoHeading);
-}
-
-void sailServoSet(int winch_amount){
-    sailServo.write(winch_amount);
-}
 
 void setup() {
     Serial.begin(9600);
@@ -47,6 +21,39 @@ void setup() {
     sailServo.write(90);
     
     //Serial.println("Ready when you are!");
+}
+
+
+//Sensor & actuator read/write functions:
+void compassRead(){
+	int compassRead=88;
+	Serial.println("$c"+String(compassRead));
+}
+
+void irRead(){
+    int irSensor = 32; //IR sensor is on Arduino Analog pin 0
+    int irReading=analogRead(irSensor);
+    
+    Serial.println("$i"+String(irReading));
+}
+
+void gyroscopeRead(){
+	//TODO: Add in I2C read gyro code
+	int gyroReading=77;
+	Serial.println("$g"+String(gyroReading));
+}
+
+String windRead(){
+	int windDirection=50; //TODO: actually read in wind direction
+	Serial.println("$d"+String(windDirection));
+}
+
+void steerServoSet(int steerServoHeading){ 
+    steerServo.write(steerServoHeading);
+}
+
+void sailServoSet(int winch_amount){
+    sailServo.write(winch_amount);
 }
 
 void read_line(char *line) {
@@ -82,43 +89,35 @@ void loop() {
     String gyroReading;
     String windDirction;
     
-    Serial.println(current_line);
+    //Serial.println(current_line);
     
     //Serial.println("in loop!");
     switch (current_line[0]){
-        case 'c': // compass
-          Serial.println("Got a c, reading from compass");
+        case 'c': // compass:
           compassRead();
           break;
-        case 'i': // infrared
-          //Serial.println("Got an i, reading from IR range 1");
-          irReading = irRead();
-          Serial.println("i"+String(irReading));
+        case 'i': // infrared:
+          irRead();
           break;
         case 'g': // gyro
-          //TODO: Add in I2C read gyro code
-          //Serial.println("Got a g, reading from gyroscope");
-          gyroReading=gyroscopeRead();
-          Serial.println("g"+String(gyroReading));
+          gyroscopeRead();
           break;
         case 'd': // wind direction
           //Serial.println("got a d, reading wind direction");
-          windDirction=windRead();
-          Serial.println("d"+String(windDirction));
+          windRead();
           break;
         case 's': // steer actuator - expects "s1232" or some other number after 's'
           amount = get_amount(current_line);
-          //Serial.print("Got a s, setting stearing servo to ");
-          Serial.println("ss"+String(amount));
+          //Serial.println("ss"+String(amount));
           steerServoSet(amount);
           break;
         case 'w': // winch actuator - expects a string with the same format as stear aervo  
           amount = get_amount(current_line);
           //Serial.print("got a w, setting sail winch to ");
-          Serial.println("sw"+String(amount));
+          //Serial.println("sw"+String(amount));
           steerServoSet(amount); //sailServo.write(amount);
           break;
         case 'h':
-          Serial.println("help mode");          
+          Serial.println("$hoi");          
     }
 }
