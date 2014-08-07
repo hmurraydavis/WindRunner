962f0efaec526604ec476d4b7f89c69365913343
Halie Murray-Davis <hmurraydavis@yahoo.com>
2014-08-07 11:19:11 -0400

    SERIAL WORKS! They send and receive!

--- Together-versions/trialHinbrainSerialIsolate.py
+++ Together-versions/trialHinbrainSerialIsolate.py
@@ -4,48 +4,8 @@ import time
 #Arduino serial connection
 arduino=serial.Serial('/dev/ttyACM0')
 
-#send = 'd\n'
-#arduino.write(send)
 
-def receiving0(ser):
-    global last_received
 
-    buffer = ''
-    last_received='\n'
-    while last_received=='\n':
-        buffer = buffer + ser.read(ser.inWaiting())
-        if '\n' in buffer:
-            lines = buffer.split('\n') # Guaranteed to have at least 2 entries
-            last_received = lines[-2]
-            #If the Arduino sends lots of empty lines, you'll lose the
-            #last filled line, so you could make the above statement conditional
-            #like so: if lines[-2]: last_received = lines[-2]
-            print last_received
-            buffer = lines[-1]
-            
-def receivingE(ser):
-    buffer = ''
-    while True:
-        buffer = buffer + ser.read(ser.inWaiting())
-        if '\n' in buffer:
-            lines = buffer.split('\n')
-            return lines[-2]
-            
-            
-def receivingSO(ser):
-    global last_received
-
-    buffer = ''
-    while True:
-        buffer = buffer + ser.read(ser.inWaiting())
-        if '\n' in buffer:
-            lines = buffer.split('\n') # Guaranteed to have at least 2 entries
-            last_received = lines[-2]
-            #If the Arduino sends lots of empty lines, you'll lose the
-            #last filled line, so you could make the above statement conditional
-            #like so: if lines[-2]: last_received = lines[-2]
-            buffer = lines[-1]
-            print last_received
 
 def receiving(ser):
     buffer = ''
@@ -59,24 +19,14 @@ def receiving(ser):
             lines = buffer.split('\n')
             return lines[-2]
 
-for i in range(15):
-	print (receiving(arduino))
+print "hello, there!"
+send = 'd\n'
+arduino.write(send)
+platypus=receiving(arduino);
+print (platypus)
+
+send='c\n'
+arduino.write(send)
+print (receiving(arduino))
 
 
-def crap():
-	readIn=False
-	count=0
-	air=[]
-
-	red=arduino.read()
-	air+=red
-	red=arduino.read()
-	air+=red
-	red=arduino.read()
-	air+=red
-	red=arduino.read()
-	air+=red
-	red=arduino.read()
-	air+=red
-
-	print air
